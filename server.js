5a89f9361e89079f7821d0df2f7a6db8f3b9c0c5
JulianTinal <julian.tinal.15tvsec@gmail.com>
2024-08-16 18:23:46 -0600

    js actu

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mysql from 'mysql2';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import crypto from 'crypto';  
 import multer from 'multer'; 
 import path from 'path'; 
